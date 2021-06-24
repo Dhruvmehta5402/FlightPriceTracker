524dd3c95057b40dd0e84814eec11db3365962ac
dhruvm4 <dhruvm4@illinois.edu>
2021-06-25 01:15:18 +0530

    Sending email successfully

---
+++ flight_track.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+import smtplib
+
+
+driver = webdriver.Chrome()
+driver.get('https://www.kayak.co.in/flights/CCU-ORD/2021-08-17/2021-12-20?sort=bestflight_a')
+time.sleep(20)
+content = driver.page_source
+soup = BeautifulSoup(content)
+
+for span in soup.findAll('span', attrs={'class':'js-label js-price _itL _ibU _ibV _idj _kKW'}):
+    price = span.text[-6:]
+    print(price)
+
+price = price.replace(',', '')
+
+if int(price) > 9999 and int(price) <= 80000:
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login("", "")
+    message = price.encode('utf-8')
+    print(message)
+    s.sendmail("", "dhruvmehtakolkata@gmail.com", message)
+    print('Email has been sent')

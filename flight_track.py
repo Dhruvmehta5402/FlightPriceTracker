66c13f2ee6d94a03c4045e3699e3c45f33f7cee2
dhruvm4 <dhruvm4@illinois.edu>
2021-06-25 16:35:21 +0530

    Added constants to top

--- flight_track.py
+++ flight_track.py
@@ -4,9 +4,15 @@ import pandas as pd
 import time
 import smtplib
 
+URL = 'https://www.kayak.co.in/flights/CCU-ORD/2021-08-17/2021-12-20?sort=bestflight_a'
+PRICE_LIMIT = 80000
+SENDER_EMAIL = ''
+SENDER_PASSWORD = ''
+RECEIVER_EMAIL = 'dhruvmehtakolkata@gmail.com'
+
 
 driver = webdriver.Chrome()
-driver.get('https://www.kayak.co.in/flights/CCU-ORD/2021-08-17/2021-12-20?sort=bestflight_a')
+driver.get(URL)
 time.sleep(25)
 content = driver.page_source
 soup = BeautifulSoup(content)
@@ -20,10 +26,13 @@ price = price.replace(',', '')
 body = f"Hello! The price is now within your range: {price}. Buy it quick before it rises again."
 message = 'Subject: {}\n\n{}'.format('Flight Price', body)
 
-if int(price) > 9999 and int(price) <= 80000:
+if int(price) > 9999 and int(price) <= PRICE_LIMIT:
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
-    s.login("", "")
+    s.login(SENDER_EMAIL, SENDER_PASSWORD)
     print(message)
-    s.sendmail("", "dhruvmehtakolkata@gmail.com", message)
+    s.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, message)
     print('Email has been sent')
+    s.quit()
+
+driver.quit()
